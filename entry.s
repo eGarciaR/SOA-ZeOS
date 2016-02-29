f29ee09c55e63adfca7de5f1e5dbe97d69e2738b
alumne <e.garcia.ribera@gmail.com>
2016-03-01 00:16:54 +0100

    Keyboard interrupt, clock interrupt

--- entry.s
+++ entry.s
@@ -1,4 +1,5 @@
 # 1 "entry.S"
+# 1 "<built-in>"
 # 1 "<command-line>"
 # 1 "/usr/include/stdc-predef.h" 1 3 4
 # 1 "<command-line>" 2
@@ -11,3 +12,17 @@
 # 6 "entry.S" 2
 # 1 "include/segment.h" 1
 # 7 "entry.S" 2
+# 72 "entry.S"
+.globl keyboard_handler; .type keyboard_handler, @function; .align 0; keyboard_handler:
+ pushl %gs; pushl %fs; pushl %es; pushl %ds; pushl %eax; pushl %ebp; pushl %edi; pushl %esi; pushl %edx; pushl %ecx; pushl %ebx; movl $0x18, %edx; movl %edx, %ds; movl %edx, %es
+ movb $0x20, %al; outb %al, $0x20
+ call keyboard_service_routine
+ popl %ebx; popl %ecx; popl %edx; popl %esi; popl %edi; popl %ebp; popl %eax; popl %ds; popl %es; popl %fs; popl %gs
+ iret
+
+.globl clock_interrupt_handler; .type clock_interrupt_handler, @function; .align 0; clock_interrupt_handler:
+ pushl %gs; pushl %fs; pushl %es; pushl %ds; pushl %eax; pushl %ebp; pushl %edi; pushl %esi; pushl %edx; pushl %ecx; pushl %ebx; movl $0x18, %edx; movl %edx, %ds; movl %edx, %es
+ movb $0x20, %al; outb %al, $0x20
+ call clock_service_routine
+ popl %ebx; popl %ecx; popl %edx; popl %esi; popl %edi; popl %ebp; popl %eax; popl %ds; popl %es; popl %fs; popl %gs
+ iret
