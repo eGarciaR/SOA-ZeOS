4bfa1effc613d52237bdaed793f152140ec45c3f
e.garcia.ribera@gmail.com <e.garcia.ribera@gmail.com>
2016-04-03 15:17:14 +0200

    fork almost finished

--- sys.c
+++ sys.c
@@ -19,7 +19,7 @@
 #define LECTURA 0
 #define ESCRIPTURA 1
 
-int pid = 2;
+int pid = 1000;
 
 int check_fd(int fd, int permissions)
 {
@@ -42,22 +42,26 @@ int sys_fork(void)
 {
   int PID = -1;
   
+	/*Get a free task*/
   if (list_empty(&freequeue)) return -1; // error
   struct list_head *firstFree = list_first(&freequeue);
   list_del(firstFree);
 
+	/*Copy the parent's task union to the child*/
   union task_union * childUnion = (union task_union*)list_head_to_task_struct(firstFree);
-  copy_data(current(),&childUnion->task,sizeof(struct task_struct);
+  copy_data(current(),&childUnion->task,sizeof(struct task_struct));
  
+	/*Store process adress space*/
   allocate_DIR(&childUnion->task);
 
+	/*Searching free physical pages*/
   int pag, new_ph_pag, i;
   page_table_entry *child_PT = get_PT(&childUnion->task);
-	for (pag = 0;pag<NUM_PAG_DATA;++pag) {
-		new_ph_pag = alloc_frame();
+	for (pag=0;pag<NUM_PAG_DATA;++pag) {
+		new_ph_pag = alloc_frame(); // New page
 		if (new_ph_pag != -1) {
 			set_ss_pag(child_PT,PAG_LOG_INIT_DATA+pag,new_ph_pag);
-		} else {
+		} else { // If there aren't enough pages, we should free allocated pages
 			for (i = 0; i < pag; ++i) {
 				unsigned int frameToFree= get_frame(child_PT,PAG_LOG_INIT_DATA+pag);
 				free_frame(frameToFree);
@@ -69,7 +73,22 @@ int sys_fork(void)
 		}
 	} 
 
-  ++pid;
+	page_table_entry *parent_PT = get_PT(current());
+	for (pag=0;pag<NUM_PAG_KERNEL;++pag) {
+		set_ss_pag(child_PT,pag,get_frame(parent_PT,pag));
+	} 
+	for (pag=0;pag<NUM_PAG_CODE;++pag) {
+		set_ss_pag(child_PT,PAG_LOG_INIT_DATA+pag,get_frame(parent_PT,PAG_LOG_INIT_DATA+pag));
+	} 
+
+	for (pag=NUM_PAG_KERNEL+NUM_PAG_CODE;pag<NUM_PAG_KERNEL+NUM_PAG_CODE+NUM_PAG_DATA;++pag) {
+		set_ss_pag(parent_PT,pag+NUM_PAG_DATA,get_frame(child_PT,pag));
+		copy_data((void*)(pag<<12),(void*)((pag+NUM_PAG_DATA)<<12),PAGE_SIZE);
+		del_ss_pag(parent_PT,pag+NUM_PAG_DATA);
+	}
+	set_cr3(get_DIR(current()));
+
+	childUnion->task.PID=++pid;
   PID = pid;
   
   return PID;
