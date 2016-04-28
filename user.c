6aaab458441ddb23db2c62aa0fa3b519900f9166
alumne <e.garcia.ribera@gmail.com>
2016-04-29 00:04:10 +0200

    FIXES

--- user.c
+++ user.c
@@ -1,8 +1,11 @@
 #include <libc.h>
+#include <schedperf.h>
 
-char buff[24];
+char buff[512];
 
 int pid;
+
+int set_sched_policy(int);
 
 int __attribute__ ((__section__(".text.main")))
   main(void)
@@ -10,16 +13,49 @@ int __attribute__ ((__section__(".text.main")))
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 	
-  int res;
-  res = set_sched_policy(0);
-  runjp();
+  set_sched_policy(0);
+	//struct stats st;
+	//get_stats(getpid(),&st);
+	//itoa(st.user_ticks, buff);
+	//write(1,buff,strlen(buff));
+	//write(1,"\n",1);
+	fork();
+	fork();
+	//fork();
+  //runjp();
+	/*int child[5];
+	int i;
+	for (i=0;i<5;++i) {
+		child[i]=fork();
+		if (child[i] >0) {
+		}
+		else if (child[i]==0) {
+			struct stats st;
+			int j = 0;
+			while(j<1000) {
+				++j;
+				//write(1,"\n",1);
+			}
+			get_stats(getpid(),&st);
+			itoa(st.user_ticks, buff);
+			write(1,"hijo:",7);
+			write(1,buff,strlen(buff));
+			write(1,"\n",1);
+			break;
+		}	
+		else if (child[i] < 0) {
+			write(1,"UUUUU",5);
+			perror();
+			exit();
+		}
+	}*/
   while(1) {
-    //itoa(getpid(),buff);
-    //write(1,buff,strlen(buff));
-    //write(1,"\n",strlen("\n"));
-	//itoa(gettime(),buff);
-  //write(1,buff,strlen(buff));	
-	//write(1,"\n",strlen("\n"));
+		itoa(getpid(),buff);
+    write(1,buff,strlen(buff));
+    write(1,"\n",strlen("\n"));
+	  //itoa(gettime(),buff);
+    //write(1,buff,strlen(buff));	
+		//write(1,"\n",strlen("\n"));
 	};
   return 0;
 }
